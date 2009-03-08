42ac88f5ef282d8f94e1a411157ff374fa758ad3
Bluebottle <isobel.hooper@gmail.com>
2009-03-08 01:46:18 +0000

    Added skeletons for all needed methods in DnaToRna class, sorted comments to be before relevant method headers, and added if(finish) break; checks at several points.
    As a result of that last one, resolved problem with DnaToRna.nat() not compiling.

--- DnaToRna.java
+++ DnaToRna.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import org.ahmadsoft.ropes.Rope;
 import org.ahmadsoft.ropes.RopeBuilder;
 
@@ -7,7 +9,13 @@ public class DnaToRna {
 	  private static RopeBuilder rb = new RopeBuilder();
 	  public static final Rope e = rb.build("");
 	  private Rope DNA = e;
+	  private Rope RNA = e;
 	  private boolean finish = false;
+	  
+	  public DnaToRna(String input)
+	  {
+		  this.DNA = rb.build(input);
+	  }
 	  
 	  public void execute()
 	  {
@@ -16,14 +24,35 @@ public class DnaToRna {
 	    while(!finish)
 	    {
 	      // Define a pattern type, set it to p.
-	      Rope p = pattern(); // (placeholder)
+	      Rope p = pattern();
+	      if(finish) break;
 	      // Define a template type, set it to t.
-	      Rope t = template(); // (placeholder)
+	      Rope t = template();
+	      if(finish) break;
 	      matchreplace(p,t);
 	    }
 	    finish();
 	  }
-
+	  
+	  /*
+	   * What methods do we need?
+	   */
+	  
+	  /*
+	   * - pattern() : [Specifies a pattern for pattern-matching]
+	   *     Loop through DNA until we reach a code telling us it's the end of the pattern, 
+	   *     or until we reach an unrecognised code (this means we trash the pattern and go
+	   *     to output the RNA), or until we run out of DNA (again, trash the pattern and go 
+	   *     to output the RNA).
+	   *      As we process, store the results of our processed DNA in one place (the pattern)
+	   *     and remove what we've processed from the DNA string.
+	   *     
+	   *     TODO: Decide between using regex notation or custom notation; if custom, document.
+	   *     
+	   *     CHOSEN: Go for regex notation at the moment. If it turns out to be unsustainable,
+	   *     reconsider and replan; for now, we'll just match it with the built in Ropes regex
+	   *     matcher in matchreplace().
+	   */
 	  public Rope pattern()
 	  {
 	    Rope p = e;
@@ -59,6 +88,7 @@ public class DnaToRna {
 	              // Interpret the next thing in the DNA string
 	              // as a natural number.
 	              int n = nat();
+	              if (finish) break;
 	              // Add an instruction to p -
 	              // "skip the next n bases".
 	              // We can do this with the regex ".{n}".
@@ -100,32 +130,78 @@ public class DnaToRna {
 	                  break;
 	                default:
 	                  finish = true;
-	                  finish();
 	              }
 	              break;
 	            default:
 	              finish = true;
-	              finish();
 	          }
 	          break;
 	        default:
 	          finish = true;
-	          finish();
 	      }
 	    }
 	    return e;
 	  }
 
+	  /*
+	   * - template() : [Specifies a template to be matched for pattern-matching]
+	   *     Loop through DNA until we reach a code telling us it's the end of the template,
+	   *     or until we reach an unrecognised code (this means we trash the template and go
+	   *     to output the RNA), or until we run out of DNA (again, trash the template and go
+	   *     to output the RNA).
+	   *      As we process, store the results of our processed DNA in one place (the template)
+	   *     and remove what we've processed from the DNA string.
+	   */
 	  private Rope template()
 	  {
 	    return e;
 	  }
 
+	  /*
+	   * - matchreplace(p,t) : [Attempts to match a pattern to part of the DNA string then replace
+	   *                        appropriate parts of the match with a template]
+	   *     Loop through the supplied pattern, keeping track of the parts it is comparing it to
+	   *     in the DNA, until we reach a part of the pattern that does not match (stop the
+	   *     matching and return to processing pattern-template-matchreplace).
+	   *      As we process, keep track of where we are in the DNA string. When we come to the start
+	   *     of groups (parts of the pattern where ( starts and ) ends them), store the index of the
+	   *     start of that group; when we come to the end of groups, add the string of bases that 
+	   *     were in that group to a list of "environments" that will be modified.
+	   *      If we get through the whole pattern without stopping the matching, remove what we've
+	   *     processed from the DNA string, and replace the environments with the templates using
+	   *     replace().
+	   */
 	  private void matchreplace(Rope pat, Rope t)
 	  {
 
 	  }
 	  
+	  /*
+	   * - replace(t,e) : [Uses the contents of matched base strings to make a replacement DNA string
+	   *                   to prepend to the existing DNA string]
+	   *     Loop through the supplied template.
+	   *      As we process, keep track of what we need to add to the replacement; this will either be
+	   *     bases, repeatedly-quoted versions of matched base strings, or lengths of matched base
+	   *     strings encoded as DNA.
+	   *      Once we have got through the supplied template, prepend the DNA to be added to the
+	   *     existing DNA string.
+	   */
+	  private void replace(Rope t,ArrayList<Rope> e)
+	  {
+		  
+	  }
+	  
+	  /*
+	   * - nat() : [Decodes a natural number]
+	   *     Loop through DNA until we reach a code telling us it's the end of the number
+	   *     we're reading, or until we run out of DNA (trash the pattern and number and go
+	   *     to output the RNA).
+	   *      As with pattern(), as we process, store the results of our processed DNA in one 
+	   *     place (the number) and remove what we've processed from the DNA string.
+	   *      TODO: Figure out how to store the number in a way that doesn't involve recursive
+	   *     calls to the same method (even though this is the way it is specified, it's unlikely
+	   *     to be the best idea).
+	   */
 	  private int nat()
 	  {
 	    char charStart = DNA.charAt(0);
@@ -143,68 +219,25 @@ public class DnaToRna {
 	        return(2*nat())+1;
 	      default:
 	        finish = true;
-	        finish();
+	        return 0;
 	    }
 	  }
 
 	  /*
-	   * What methods do we need?
-	   * 
-	   * - pattern() : [Specifies a pattern for pattern-matching]
-	   *     Loop through DNA until we reach a code telling us it's the end of the pattern, 
-	   *     or until we reach an unrecognised code (this means we trash the pattern and go
-	   *     to output the RNA), or until we run out of DNA (again, trash the pattern and go 
-	   *     to output the RNA).
-	   *      As we process, store the results of our processed DNA in one place (the pattern)
-	   *     and remove what we've processed from the DNA string.
-	   * 
-	   * - template() : [Specifies a template to be matched for pattern-matching]
-	   *     Loop through DNA until we reach a code telling us it's the end of the template,
-	   *     or until we reach an unrecognised code (this means we trash the template and go
-	   *     to output the RNA), or until we run out of DNA (again, trash the template and go
-	   *     to output the RNA).
-	   *      As we process, store the results of our processed DNA in one place (the template)
-	   *     and remove what we've processed from the DNA string.
-	   * 
-	   * - matchreplace(p,t) : [Attempts to match a pattern to part of the DNA string then replace
-	   *                        appropriate parts of the match with a template]
-	   *     Loop through the supplied pattern, keeping track of the parts it is comparing it to
-	   *     in the DNA, until we reach a part of the pattern that does not match (stop the
-	   *     matching and return to processing pattern-template-matchreplace).
-	   *      As we process, keep track of where we are in the DNA string. When we come to the start
-	   *     of groups (parts of the pattern where ( starts and ) ends them), store the index of the
-	   *     start of that group; when we come to the end of groups, add the string of bases that 
-	   *     were in that group to a list of "environments" that will be modified.
-	   *      If we get through the whole pattern without stopping the matching, remove what we've
-	   *     processed from the DNA string, and replace the environments with the templates using
-	   *     replace().
-	   *     
-	   * - replace(t,e) : [Uses the contents of matched base strings to make a replacement DNA string
-	   *                   to prepend to the existing DNA string]
-	   *     Loop through the supplied template.
-	   *      As we process, keep track of what we need to add to the replacement; this will either be
-	   *     bases, repeatedly-quoted versions of matched base strings, or lengths of matched base
-	   *     strings encoded as DNA.
-	   *      Once we have got through the supplied template, prepend the DNA to be added to the
-	   *     existing DNA string.  
-	   * 
-	   * - nat() : [Decodes a natural number]
-	   *     Loop through DNA until we reach a code telling us it's the end of the number
-	   *     we're reading, or until we run out of DNA (trash the pattern and number and go
-	   *     to output the RNA).
-	   *      As with pattern(), as we process, store the results of our processed DNA in one 
-	   *     place (the number) and remove what we've processed from the DNA string.
-	   *      TODO: Figure out how to store the number in a way that doesn't involve recursive
-	   *     calls to the same method (even though this is the way it is specified, it's unlikely
-	   *     to be the best idea).
-	   * 
 	   * - asnat() : [Encodes a natural number]
 	   *     Take a natural number and returns a representation of it encoded in DNA form.
 	   *      Numbers are in binary with most significant bit last, and terminated with P.
 	   *     So, for example, decimal 10 (binary 1010) would be stored as ICICP, and decimal 25
 	   *     (binary 11001) would be stored as CIICCP.
+	   *     
 	   *      TODO: As with nat(), figure out a non-recursive way of doing this.
-	   * 
+	   */
+	  private Rope asnat(int n)
+	  {
+		  return e;
+	  }
+	  
+	  /*
 	   * - consts() : [Decodes a sequence of bases]
 	   *     Loop through DNA until we reach a code we don't recognise or until we run out of DNA
 	   *     (return to whatever called this method).
@@ -212,15 +245,60 @@ public class DnaToRna {
 	   *     in one place (the decoded DNA) and remove what we've processed from the DNA string.
 	   *      TODO: As with nat(), figure out how to store the decoded DNA in a way that doesn't
 	   *     involve recursive calls to the same method.
-	   * 
+	   */
+	  private Rope consts()
+	  {
+	    char charFirst = DNA.charAt(0);
+	    switch (charFirst)
+	    {
+	      case 'C':
+	        DNA = DNA.delete(0,0);
+	        return consts().insert(0,"I");
+	      case 'F':
+	        DNA = DNA.delete(0,0);
+	        return consts().insert(0,"C");
+	      case 'P':
+	        DNA = DNA.delete(0,0);
+	        return consts().insert(0,"F");
+	      case 'I':
+	        char charSecond = DNA.charAt(1);
+	        switch (charSecond)
+	        {
+	          case 'C':
+	            DNA = DNA.delete(0,1);
+	            return consts().insert(0,"P");
+	          default:
+	            return e;
+	        }
+	      default:
+	        return e;
+	    }
+	  }
+
+	  /*
 	   * - protect(l,d) : [Repeatedly encodes a sequence of bases using quote()]
 	   *     Call the quote() method on d repeatedly until it's been done l times. (This is in the 
 	   *     spec as recursive, but can easily be made iterative.)
-	   * 
+	   */
+	  private void protect(int l, Rope d)
+	  {
+		  
+	  }
+	  
+	  /*
 	   * - quote() : [Encode a sequence of bases]
 	   *     Go through all the bases in a DNA string, turning them into their "quoted" forms. (As
 	   *     for protect(), this is in the spec as recursive but can easily be made iterative.)
 	   */
+	  private Rope quote(Rope d)
+	  {
+		  return e;
+	  }
+	  
+	  private void finish()
+	  {
+	    // Outputs RNA string.
+	  }
 
 	
 }
