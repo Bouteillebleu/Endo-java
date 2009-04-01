b6363986338370c8e9559a8c75e84f3836129ef6
Bluebottle <isobel.hooper@gmail.com>
2009-04-01 16:51:58 +0000

    Created GenomeReader class. Also added a text file with some translations of the short prefixes.
    Briefly tried to make it a GUI app, but apparently javax.swing.JTextArea doesn't like being filled with 7.5 million character strings (the lightweight), so am going to think of a better way to display the text, whether GUI or not. Possibly just displaying certain chunks at a time in some sort of buffer.
    
    TODO: Think of buffered solutions to the text size problem. Also plan out what GenomeReader should mark / annotate / do.

---
+++ source/GenomeReader.java
@@ -0,0 +1,19 @@
+package source;
+
+public class GenomeReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//GenomeReader gr = new GenomeReader();
+
+	}
+	
+	public GenomeReader()
+	{
+
+	}
+
+}
